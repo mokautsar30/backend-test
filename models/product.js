7d3dd9b475ff16fc45769c69f1fcf2a07a2d0669
mohammad kautsar al <mohammadkautsar30@gmail.com>
2024-04-29 00:27:22 +0700

    feat: database migration

---
+++ models/product.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    static associate(models) {
+      Product.belongsTo(models.User, {
+        foreignKey: 'userId'
+      })
+    }
+  }
+  Product.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Product name is required"
+        },
+        notEmpty: {
+          msg: "Product name is required"
+        }
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
